d581d4c760ad3266fc05531f3eb2a054799644b1
Sarah Smith <155038730+sarahlibx@users.noreply.github.com>
2025-12-12 14:23:15 -0500

    Initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
+
+// GET landing page for sign up
+router.get("/sign-up", (req, res) => {
+  res.render("auth/sign-up.ejs");
+});
+
+// POST create a new account w/checks for username & password validation
+router.post("/sign-up", async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (userInDatabase) {
+        return res.send("Username already taken.");
+    }
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send("Password and Confirm Password must match");
+    }
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+        req.body.password = hashedPassword;
+
+    const user = await User.create(req.body);
+    // res.send(`Thanks for signing up ${user.username}`);
+
+    req.session.user = {
+    username: user.username,
+    };
+
+    req.session.save(() => {
+    res.redirect("/");
+    });
+});
+
+// GET sign in page
+router.get("/sign-in", (req, res) => {
+  res.render("auth/sign-in.ejs");
+});
+
+// POST sign in w/checks for username & password validation
+router.post("/sign-in", async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+
+    if (!userInDatabase) {
+        return res.send("Login failed, user not found.");
+    }
+
+    const validPassword = bcrypt.compareSync(
+        req.body.password,
+        userInDatabase.password
+    );
+
+    if (!validPassword) {
+        return res.send("Login failed. Invalid password.");
+    }
+
+    req.session.user = {
+        username: userInDatabase.username,
+    };
+
+    req.session.save(() => {
+    res.redirect("/");
+    });
+});
+
+// GET sign out page
+router.get("/sign-out", (req, res) => {
+    req.session.destroy();
+    // clear cookie session
+    res.clearCookie("connect.sid", {
+            path: "/", 
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production"
+        });
+
+    res.redirect("/");
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,60 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const session = require('express-session');
+const MongoStore = require("connect-mongo");
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
+
+// Set the port from environment variable or default to 3000
+const port = process.env.PORT ? process.env.PORT : "3000";
+
+const authController = require("./controllers/auth.js");
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// Middleware to parse URL-encoded data from forms
+app.use(express.urlencoded({ extended: false }));
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride("_method"));
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
+// middleware that creates a session for every request
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
+// middleware that makes the user available to all view routes
+app.use(passUserToView);
+
+// GET /landing page
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
+});
+
+// GET VIP lounge access
+app.get("/vip-lounge", isSignedIn, (req, res) => {
+    res.send(`Welcome to the party ${req.session.user.username}.`);
+});
+
+// auth routes
+app.use("/auth", authController);
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
